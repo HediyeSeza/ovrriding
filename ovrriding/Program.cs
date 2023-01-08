9216017e23f332e980176b43095b51176dce45a0
sezahedi <sezahedi13@gmail.com>
2023-01-08 10:28:29 +0330

    Add project files.

---
+++ ovrriding/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ovrriding
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog d = new Dog();
+            d.voise();
+            Console.ReadKey();
+        }
+    }
+    public class Animal
+    {
+        public virtual void voise()
+        {
+            Console.WriteLine("Animal");
+        }
+    }
+    public class Dog : Animal
+    {
+        public override void voise()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+}
